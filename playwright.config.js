0cf8a2d7082e01490930b27eaa3cb7d199262a55
Ekanto <omar.faruq@vml.com>
2025-10-02 22:01:55 +0600

    Removed webdkit

--- playwright.config.js
+++ playwright.config.js
@@ -51,10 +51,6 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
 
     /* Test against mobile viewports. */
     // {
