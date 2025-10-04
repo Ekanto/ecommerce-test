c8aed4cb6a711546a9b755e4331a128f00f4f9e1
Ekanto <omar.faruq@vml.com>
2025-10-04 15:24:54 +0600

    - Update yml - Added test suite

--- playwright.config.js
+++ playwright.config.js
@@ -45,35 +45,15 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
+   projects: [
     {
       name: 'chromium',
-      use: { 
-        viewport: null, // Disable default viewport to allow --start-maximized to work
-      },
+      use: { ...devices['Desktop Chrome'] },
     },
-
-
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
   ],
 
     outputDir: 'test-results/',
